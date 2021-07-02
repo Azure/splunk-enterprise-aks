db95e76ed42c5f9c802f59e5763a2140490b97de
dwrightmsft <darren.wright@microsoft.com>
2021-07-02 14:39:59 +0100

    Update minio.sh

--- scripts/minio.sh
+++ scripts/minio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download and install Helm
-wget -O helm.tgz https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+wget -O helm.tgz https://get.helm.sh/helm-v3.5.4-linux-amd64.tar.gz
 tar -zxvf helm.tgz
 mv linux-amd64/helm /usr/local/bin/helm
 
